0d7446bc51ccfc14cae54539587e1fb86dc0e8ed
Prajeet Shrestha <pj.shrestha@andmine.com>
2015-03-23 12:58:03 +0545

    Added app delegate

---
+++ PJAlertDemo/PJAlertDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PJAlertDemo
+//
+//  Created by Prajeet Shrestha on 3/23/15.
+//  Copyright (c) 2015 Prajeet Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
